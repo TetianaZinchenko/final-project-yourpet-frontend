17d0cdf00ac86090b91101d06cfcfeed84a043ea
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-05-20 20:03:44 +0300

    added theme.js

---
+++ theme/theme.js
@@ -0,0 +1,40 @@
+export const theme = Object.freeze({
+  fonts: {
+    main: `'Manrope', sans-serif`,
+    second: `'Inter', sans-serif`,
+  },
+  fontSizes: [12, 14, 16, 18, 20, 24, 28, 32, 44, 48, 60, 72, 100],
+  fontWeights: {
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+  },
+  colors: {
+    accent: '#FF8356',
+    background: '#FEF9F9',
+    black: '#111111',
+    yellow: '#FFC107',
+    blue: '#54ADFF',
+    blueGradient: 'linear-gradient(290.46deg, #419EF1 0%, #9BD0FF 107.89%)',
+    blueLight: '#CCE4FB',
+    red: '#F43F5E',
+    green: '#00C3AD',
+    grey: '#888888',
+    white: '#FFFFFF',
+    orangeGradient: 'linear-gradient(90deg, #FF634E 0%, #FFDF48 105.44%)',
+    backdrop: 'rgba(97, 97, 97, 0.6)',
+    transparent: 'transparent',
+  },
+  shadows: {
+    defaultShadow: '3px 8px 14px rgba(136, 198, 253, 0.19)',
+    hoverShadow: '7px 13px 14px rgba(116, 177, 232, 0.24)',
+  },
+  media: {
+    mobile: '(min-width: 320px)',
+    tablet: '(min-width: 768px)',
+    desktop: '(min-width: 1280px)',
+    mobileTablet: '(min-width: 320px) and (max-width: 761px)',
+    tabletDesktop: '(min-width: 768px) and (max-width: 1279px)',
+  },
+});
