061a1272fc3c634a3f8d66aced0e8640074eccd5
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-05-21 16:21:37 +0300

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { Routes } from 'react-router-dom';
 
 import { ThemeProvider } from '@emotion/react';
 
-import { theme } from '../theme/theme.js';
+import { theme } from '../theme/theme';
 
 export const App = () => {
   return (
