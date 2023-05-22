b351e91adb0aee2437d10d9d213e7210bc54e459
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-05-22 10:05:39 +0300

    added lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
-// import { ThemeProvider } from '@emotion/react';
-// import { theme } from '../theme/theme';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { lazy } from 'react';
+
+import { ThemeProvider } from '@emotion/react';
+import { theme } from '../theme/theme';
+
 import { SharedLayout } from './SharedLayout/SharedLayout';
-import { MainPage } from '../pages/MainPage';
-import { RegisterPage } from 'pages/RegisterPage';
-import { LoginPage } from 'pages/LoginPage';
-import { NoticesPage } from 'pages/NoticesPage';
-import { UserPage } from 'pages/UserPage';
-import { AddPetPage } from 'pages/AddPetPage';
+
+const MainPage = lazy(() => import('../pages/MainPage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage'));
+const LoginPage = lazy(() => import('../pages/LoginPage'));
+const NoticesPage = lazy(() => import('../pages/NoticesPage'));
+const UserPage = lazy(() => import('../pages/UserPage'));
+const AddPetPage = lazy(() => import('../pages/AddPetPage'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
 
 export const App = () => {
   return (
-    // <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route path="/main" element={<MainPage />} />
@@ -21,8 +26,12 @@ export const App = () => {
           {/* Privat route */}
           <Route path="/user" element={<UserPage />} />
           <Route path="/add-pet" element={<AddPetPage />} />
+          <Route
+            path="*"
+            element={<Navigate to="404" component={<NotFoundPage />} />}
+          />
         </Route>
       </Routes>
-    // </ThemeProvider>
+    </ThemeProvider>
   );
 };
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <>
+      <h2>NotFound 404</h2>
+    </>
+  );
+};
+
+export default NotFoundPage;
