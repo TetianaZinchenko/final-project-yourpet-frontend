18f0f8d40e55bfbdc54384c06b20b1e0437d914e
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-05-23 09:26:08 +0000

    Deploying to gh-pages from @ TetianaZinchenko/final-project-yourpet-frontend@fa5942b3e77637b8358fee954507022741293e36 🚀

--- static/js/520.a68d9419.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfinal_project_yourpet_frontend=self.webpackChunkfinal_project_yourpet_frontend||[]).push([[520],{4520:function(n,e,r){r.r(e);var t=r(184);e.default=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("h2",{children:"NotFound 404"})})}}}]);
-//# sourceMappingURL=520.a68d9419.chunk.js.map
