3d44f9859ffc0f0aeb4f8f975a9dd675b59af919
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-05-23 09:42:44 +0300

    added HeroImg and some styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,28 @@
 import { ContainerWithBg } from 'components/ContainerWithBg/ContainerWithBg';
 // import DeskImgHero from '../../images/home_desktop.png'
-import { Title, Wrap } from './Hero.styled';
+import { HeroImg, Title, Wrap } from './Hero.styled';
+
+// import home_desktop_1x from '../../images/home_img/home_desktop@1x.png';
+// import home_desktop_2x from '../../images/home_img/home_desktop@2x.png';
+import home_tablet_1x from '../../images/home_img/home_tablet@1x.png';
+import home_tablet_2x from '../../images/home_img/home_tablet@2x.png';
+import home_mobile_1x from '../../images/home_img/home_mobile@1x.png';
+import home_mobile_2x from '../../images/home_img/home_mobile@2x.png';
 
 export const Hero = () => {
   return (
     <ContainerWithBg>
-       <Wrap> 
+      <Wrap>
         <Title>Take good care of your small pets</Title>
         {/* <HeroImg src={DeskImgHero} alt="" /> */}
+
+        <HeroImg
+          srcSet={`${home_mobile_1x} 320w, ${home_mobile_2x} 640w, ${home_tablet_1x} 768w, ${home_tablet_2x} 1536w`}
+          src={home_mobile_1x}
+          sizes="(max-width: 767px) 320px, (max-width: 767px) 640px, (min-width: 768px) 768px, (min-width: 768px) 1536px, 100vw"
+          alt="hero"
+          loading="lazy"
+        />
       </Wrap>
     </ContainerWithBg>
   );
--- src/components/Hero/Hero.styled.js
+++ src/components/Hero/Hero.styled.js
@@ -3,51 +3,69 @@ import styled from '@emotion/styled';
 export const Wrap = styled.div`
   width: 100%;
   height: 100%;
+
   padding-top: 60px;
+
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
 
-  @media screen and (min-width: 768px) {
+  @media screen and (${props => props.theme.media.tablet}) {
     padding-top: 80px;
   }
-  
-  @media screen and (min-width: 1280px) {
+
+  @media screen and (${props => props.theme.media.desktop}) {
     padding-top: 30px;
     flex-direction: row;
     justify-content: flex-start;
     align-items: center;
   }
-
-
-
 `;
 
 export const Title = styled.h1`
   width: 100%;
-  font-weight: 700;
+
+  /* font-weight: 700; */
+  font-weight: ${props => props.theme.fontWeights.bold};
   font-size: 32px;
   line-height: calc(44 / 32);
+
   margin: 0;
 
-  @media screen and (min-width: 768px) {
+  @media screen and (${props => props.theme.media.tablet}) {
     font-size: 68px;
     line-height: calc(100 / 68);
   }
 
-  @media screen and (min-width: 1280px) {
-    max-width: 588px;
-    font-weight: 800;
+  @media screen and (${props => props.theme.media.desktop}) {
+    /* max-width: 588px; */
+    max-width: 501px;
+
+    font-weight: ${props => props.theme.fontWeights.extrabold};
+    /* font-weight: 800; */
     font-size: 68px;
     line-height: calc(88 / 68);
   }
 `;
 
+export const HeroImg = styled.img`
+  width: 320px;
+  height: 391px;
 
-// export const HeroImg = styled.img`
-//    position: relative;
-//    margin-left: -200px;
-//    width: 917px;
-//    align-self: flex-end;
-// `
+  margin-top: 20px;
+
+  @media screen and (${props => props.theme.media.tablet}) {
+    width: 768px;
+    height: 823px;
+
+    margin-top: 27px;
+  }
+
+  @media screen and (${props => props.theme.media.desktop}) {
+    width: 917px;
+    height: 670px;
+
+    margin-top: 0;
+  }
+`;
