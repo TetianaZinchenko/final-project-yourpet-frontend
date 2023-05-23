e7eefe1170ed5a7f77947757f57fbd38cb529e4e
reebass <store1993dron@gmail.com>
2023-05-23 17:28:29 +0300

    section components

--- src/components/ContainerWithBg/ContainerWithBg.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./ContainerWithBg.styled"
-
-export const ContainerWithBg = ({children}) => {
-    return (
-        <Container>
-            {children}
-        </Container>
-    )
-}
