27adf2754c12e3b9c6ffd2ae0a3268bb50d36663
MythosOne <107143884+MythosOne@users.noreply.github.com>
2023-05-24 23:13:52 +0300

    NoticesSearch- NoticesCategoriesNav

---
+++ src/components/Notices/Notices.jsx
@@ -0,0 +1,12 @@
+import  {NoticesSearch}  from './NoticesSearch/NoticesSearch';
+import { NoticesCategoriesNav } from './NoticesCategoriesNav/NoticesCategoriesNav';
+
+export const Notices = () => {
+  return (
+    <>
+    <NoticesSearch />
+    <NoticesCategoriesNav/>
+    </>
+  
+  );
+};
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -6,6 +6,7 @@ export const SharedLayout = () => {
   return (
     <>
       <Header />
+      <Notices/>
       <main>
         <Suspense fallback={null}>
           <Outlet />
