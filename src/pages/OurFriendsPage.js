aff19ddcb3ca69248c69b04ae570f524fa00eece
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-05-24 17:49:34 +0300

    fixed

--- src/pages/OurFriendsPage.js
+++ src/pages/OurFriendsPage.js
@@ -1,23 +1,13 @@
 import OurFriends from '../components/OurFriends/OurFriends';
 import friends from '../components/OurFriends/sponsors.json';
 
-
-
 const OurFriendsPage = () => {
-  
- 
-
-  
   return (
     <>
-      
-      <h1>Our Friends</h1>
-      <OurFriends friends={friends}
-        />
-        
+      <h2>Our Friends</h2>
+      <OurFriends friends={friends} />
     </>
-  )
-
+  );
 };
 
 export default OurFriendsPage;
