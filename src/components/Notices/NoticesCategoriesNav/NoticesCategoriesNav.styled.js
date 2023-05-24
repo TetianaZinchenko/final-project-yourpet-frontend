523fc60a63f86c6093b9ef7daf1a76830b68858b
MythosOne <107143884+MythosOne@users.noreply.github.com>
2023-05-24 23:42:41 +0300

    NoticesSearch-NoticesCategoriesNav-jsx-styled

--- src/components/Notices/NoticesCategoriesNav/NoticesCategoriesNav.styled.js
+++ src/components/Notices/NoticesCategoriesNav/NoticesCategoriesNav.styled.js
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
-import {theme} from 'theme/theme';
+import { theme } from 'theme/theme';
 
 export const List = styled.ul`
   display: flex;
@@ -22,14 +22,18 @@ export const Button = styled(NavLink)`
 
   text-decoration: none;
 
+  font-family: ${theme.fonts.main};
+  font-weight:  ${theme.fontWeights.medium};
+  font-size: ${theme.fontSizes[1]}px;
+
   color: ${theme.colors.blue};
 
   background-color: ${theme.colors.blueLight};
   border: none;
   border-radius: 40px;
 
-  transition: color 300ms cubic-bezier(0.4, 0, 0.2, 1),
-    background-color 300ms cubic-bezier(0.4, 0, 0.2, 1);
+  transition: color 300ms ${theme.shadows.defaultShadow},
+    background-color 300ms ${theme.shadows.defaultShadow};
 
   &.active {
     color: ${theme.colors.white};
@@ -52,7 +56,7 @@ export const Button = styled(NavLink)`
 
     z-index: -1;
 
-    transition: opacity 300ms cubic-bezier(0.4, 0, 0.2, 1);
+    transition: opacity 300ms ${theme.shadows.defaultShadow};
   }
 
   &:hover::before,
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,6 +1,7 @@
 import { Header } from 'components/Header/Header';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import {Notices} from 'components/Notices/Notices'
 
 export const SharedLayout = () => {
   return (
