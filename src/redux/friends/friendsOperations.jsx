6368f718eb0db0392dbabb99fa1443274b258876
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-05-29 19:22:04 +0300

    fixed

--- src/redux/friends/friendsOperations.jsx
+++ src/redux/friends/friendsOperations.jsx
@@ -5,7 +5,9 @@ export const fetchFriends = createAsyncThunk(
   'friends/fetchFriends',
   async (_, { rejectWithValue }) => {
     try {
-      const response = await axios.get(`https://final-project-yourpe-backend.onrender.com/friends/news.json`);
+      const response = await axios.get(
+        `https://final-project-yourpe-backend.onrender.com/friends/news.json`
+      );
       return response.data;
     } catch (error) {
       return rejectWithValue(error.message);
