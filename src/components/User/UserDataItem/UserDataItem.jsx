d9d3173679a48831934c5d93efc3b3a7d264e382
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-05-31 22:57:09 +0300

    fixed

--- src/components/User/UserDataItem/UserDataItem.jsx
+++ src/components/User/UserDataItem/UserDataItem.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUser, updateUser } from '../../../redux/auth/authOperations';
 import { selectUser, selectAuth } from '../../../redux/auth/authSelectors';
