33d98bb2d6eabd90290047802fd518d96807bffa
Vita Filatova <filatova.vitta@gmail.com>
2023-06-02 02:18:03 +0300

    Update newsOperations.js

--- src/redux/news/newsOperations.js
+++ src/redux/news/newsOperations.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://final-project-yourpe-backend.onrender.com';
+
 
 export const fetchNews = createAsyncThunk(
   '/news/fetchNews',
   async (_, { rejectWithValue }) => {
     try {
-      const response = await axios.get('/news');
+      const response = await axios.get(`https://final-project-yourpe-backend.onrender.com/friends/news.json`);
       return response.data;
     } catch (error) {
 
