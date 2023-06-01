7f95f3e82a5fbb4eb2a74c49be38b69a61fd388f
Vita Filatova <filatova.vitta@gmail.com>
2023-06-02 02:18:01 +0300

    Update newsSelectors.js

--- src/redux/news/newsSelectors.js
+++ src/redux/news/newsSelectors.js
@@ -1,5 +1,5 @@
-export const selectNews = state => state.news.items;
+export const selectNews = ({news}) => news.items;
 
-export const selectIsLoading = state => state.news.isLoading;
+export const selectIsLoading = ({news}) => news.isLoading;
 
-export const selectError = state => state.news.error;
+export const selectError =({news}) => news.error;
