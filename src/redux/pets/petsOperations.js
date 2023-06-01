2683215ee19e83409bed2e75989f05df11ba79b6
IrynaDenysovich <106885843+IrynaDenysovich@users.noreply.github.com>
2023-06-01 14:39:53 +0300

    revert url

--- src/redux/pets/petsOperations.js
+++ src/redux/pets/petsOperations.js
@@ -2,8 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { selectToken } from 'redux/auth/authSelectors';
 
-axios.defaults.baseURL = 'http://localhost:3000';
-
+axios.defaults.baseURL = 'https://final-project-yourpe-backend.onrender.com';
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
